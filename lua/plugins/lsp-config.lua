750ebcc7bc46ea078f7bf86f3a4bd46cc21a1473
Eduardo Pascua <pascua.eduardo@gmail.com>
2024-08-05 19:09:09 +0200

    Add additional keymaps for lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,6 +24,13 @@ return {
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+      vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, {})
+      vim.keymap.set('n', 'gn', vim.lsp.buf.rename, {})
+      vim.keymap.set('n', '[g', vim.diagnostic.goto_prev, {})
+      vim.keymap.set('n', 'g]', vim.diagnostic.goto_next, {})
     end
   }
 }
